c75477b3e90731531b3663781003414bf566033a
PADMA-PRIYA281 <padmapriya260806@gmail.com>
2026-06-09 07:58:16 +0530

    Uploaded Caesar Cipher Project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# PRODIGY_CS_01
+# Caesar Cipher Program
+
+This project is a Python implementation of the Caesar Cipher algorithm.
+
+## Features
+- Encrypt messages
+- Decrypt messages
+- User input support
+- Shift value customization
+
+## Technologies Used
+- Python
+
+## How to Run
+1. Open terminal
+2. Run:
+python caesar_cipher.py
---
+++ caesar_cipher.py
@@ -0,0 +1,36 @@
+def encrypt(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            shift_base = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            result += char
+
+    return result
+
+
+def decrypt(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            shift_base = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift_base - shift) % 26 + shift_base)
+        else:
+            result += char
+
+    return result
+
+
+print("=== Caesar Cipher Program ===")
+
+message = input("Enter your message: ")
+shift = int(input("Enter shift value: "))
+
+encrypted = encrypt(message, shift)
+print("Encrypted Message:", encrypted)
+
+decrypted = decrypt(encrypted, shift)
+print("Decrypted Message:", decrypted)
